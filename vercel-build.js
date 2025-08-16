b47df52d6c6bb3c22068c5425e7e86f3a1649216
Dwi Adnyana <dwiadnyanaa04@gmail.com>
2025-08-17 00:18:23 +0800

    update

---
+++ vercel-build.js
@@ -0,0 +1,4 @@
+// This file is needed for Vercel deployment
+console.log('Skipping build process for Laravel...');
+console.log('Make sure to run `composer install` and `php artisan` commands locally before deployment.');
+console.log('Build completed successfully');
